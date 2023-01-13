ae009b10e874b55e2c5c45589496204b95016e2b
TonyYork97 <ayu2600@mail.ru>
2023-01-13 18:30:33 +0400

    change api variables

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000'
+const API_URL = 'https://web-money-backend.vercel.app'
 
 const instance = axios.create({
     withCredentials: true,
